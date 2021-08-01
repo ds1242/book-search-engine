c51c6d56bd72ac6fe717ae69c8604bfea9a2ae9e
ds1242 <david.shaw1242@gmail.com>
2021-08-01 10:45:48 -0600

    starting to implement mutations and query into front end

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,64 @@
+import gql from 'graphql-tag';
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+            login(email: $email, password: $password) {
+            token
+            user {
+                _id
+            }
+        }
+    }
+`;
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $password:String!, $email: String!) {
+        addUser(username: $username, password: $password, email: $email) {
+            token 
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook ($bookId: String!, $authors: [String], $description: String!, $title: String!, $image: String!, $link: String!) {
+        saveBook(bookId: $bookId, authors: $authors, description: $description, title: $title, image:$image, link: $link) {
+            _id
+            username
+            email
+            bookCount
+                savedBooks {
+                _id
+                title
+                bookId
+                image
+                link
+                description
+                authors
+            }
+        }        
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation deleteBook($_id: ID ) {
+        deleteBook(_id: $_id) {
+            _id
+            username
+            email
+            bookCount
+                savedBooks {
+                _id
+                title
+                bookId
+                image
+                link
+                description
+                authors
+            }
+        }
+    }
+`;
