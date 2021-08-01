979dbf03cb65c867e26c9a6d108b05a48423c0ff
ds1242 <david.shaw1242@gmail.com>
2021-08-01 14:39:03 -0600

    ADD_USER mutation added and functioning

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,7 +2,6 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from '@apollo/react-hooks';
-// import { loginUser } from '../utils/API';
 import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
@@ -10,7 +9,7 @@ const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
-  const [ login] = useMutation(LOGIN_USER);
+  const [ login ] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,18 +27,14 @@ const LoginForm = () => {
     }
     
     try {
-      const response = await login({
+      const { data } = await login({
         variables: {...userFormData}
         
       });
-      console.log(response);
       // if (!response.ok) {
       //   throw new Error('something went wrong!');
       // }
-
-      // const { token, user } = await response.json();
-      // console.log(user);
-      Auth.login(response.data.token);
+      Auth.login(data.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
